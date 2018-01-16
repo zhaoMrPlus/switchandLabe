fc47da0944a07be0ccf2be2db5897f9c937daa2b
zhaohongbin1111 <15671056747@163.com>
2018-01-16 19:58:20 +0800

    Initial Commit

---
+++ SwiftDemo2Tests/SwiftDemo2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftDemo2Tests.swift
+//  SwiftDemo2Tests
+//
+//  Created by 毛蛋 on 2018/1/16.
+//  Copyright © 2018年 zhb. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftDemo2
+
+class SwiftDemo2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
